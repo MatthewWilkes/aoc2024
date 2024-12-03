2a379bf406c13ca8021d692e8f75308801316ba7
Matthew Wilkes <git@matthewwilkes.name>
2024-12-03 20:39:00 +0000

    Borrow readings vector

--- 02/src/main.rs
+++ 02/src/main.rs
@@ -11,14 +11,14 @@ fn calculate_from_string(input: &str) -> u32 {
     let mut num_safe = 0;
     for line in input.split("\n") {
         let item_iter: Vec<i32> = line.split(" ").map(| x | x.parse().unwrap()).collect();
-        if is_safe(item_iter) {
+        if is_safe(&item_iter) {
             num_safe += 1;
         }
     };
     return num_safe;
 }
 
-fn is_safe(readings: Vec<i32>) -> bool {
+fn is_safe(readings: &Vec<i32>) -> bool {
     let differences: Vec<i32> = readings.windows(2).map(| a | a[1]-a[0]).collect();
     println!("{:?}", differences);
     if differences.iter().all(|&x| {-4 < x && x < 0}) {
@@ -41,7 +41,7 @@ mod tests {
     fn test_safe_increasing() {
         let sample = vec![1, 3, 6, 7, 9];
         assert_eq!(
-            is_safe(sample), true
+            is_safe(&sample), true
         );
     }
 
@@ -49,7 +49,7 @@ mod tests {
     fn test_unsafe_increasing() {
         let sample = vec![1, 2, 7, 8, 9];
         assert_eq!(
-            is_safe(sample), false
+            is_safe(&sample), false
         );
     }
 
@@ -58,7 +58,7 @@ mod tests {
     fn test_safe_decreasing() {
         let sample = vec![7, 6, 4, 2, 1];
         assert_eq!(
-            is_safe(sample), true
+            is_safe(&sample), true
         );
     }
 
@@ -66,7 +66,7 @@ mod tests {
     fn test_unsafe_decreasing() {
         let sample = vec![9, 7, 6, 2, 1];
         assert_eq!(
-            is_safe(sample), false
+            is_safe(&sample), false
         );
     }
 
@@ -74,7 +74,7 @@ mod tests {
     fn test_unsafe_mixed() {
         let sample = vec![1, 3, 2, 4, 5];
         assert_eq!(
-            is_safe(sample), false
+            is_safe(&sample), false
         );
     }
 
@@ -82,7 +82,7 @@ mod tests {
     fn test_unsafe_equal() {
         let sample = vec![8, 6, 4, 4, 1];
         assert_eq!(
-            is_safe(sample), false
+            is_safe(&sample), false
         );
     }
 
